5fa9ac53339e0387db9aae4dd1e4708e4a206b32
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2023-10-30 09:57:58 -0500

    firts and second testing passed

--- part1/question1.py
+++ part1/question1.py
@@ -1,10 +1,10 @@
 ################################################################################
 #     ____                          __     _                          ___
 #    / __ \  __  __  ___    _____  / /_   (_)  ____    ____          <  /
-#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \         / / 
-#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        / /  
-#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /_/   
-#                                                                        
+#   / / / / / / / / / _ \  / ___/ / __/  / /  / __ \  / __ \         / /
+#  / /_/ / / /_/ / /  __/ (__  ) / /_   / /  / /_/ / / / / /        / /
+#  \___\_\ \__,_/  \___/ /____/  \__/  /_/   \____/ /_/ /_/        /_/
+#
 #  Question 1
 ################################################################################
 #
@@ -12,24 +12,24 @@
 # The two functions below are used to tell the weather. They have some bugs that
 # need to be fixed. The test suite in `question1_test.py` will verify the output.
 # Read the test suite to know the values that these functions should return.
+city_info = {
+    "Quito": {"temperature": 22, "weather": "sunny"},
+    "Sao Paulo": {"temperature": 17, "weather": "cloudy"},
+    "San Francisco": {"temperature": 16, "weather": None},
+    "New York": {"temperature": 14, "weather": "rainy"},
+}
+
 
 def get_city_temperature(city):
-   if city == "Quito":
-      return 22
-   if city == "Sao Paulo":
-      return 17
-   if city == "San Francisco":
-      return 16
+   return city_info.get(city, {}).get("temperature")
+
+
 
 def get_city_weather(city):
+   sky_condition = city_info.get(city, {}).get("weather")
 
-  sky_condition = None
 
-  if city == "Sao Paulo":
-     sky_condition = "cloudy"
-  elif city == "New York":
-     sky_condition = "rainy"
+   temperature = get_city_temperature(city)
 
-  temperature = get_city_temperature(city)
 
-  return str(temperature) + " degrees and " + sky_condition
+   return str(temperature) + " degrees and " + sky_condition
